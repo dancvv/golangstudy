e929c418d5598cd9288cba96920a2020d5307221
weivang <uquantum@Hotmail.com>
2022-04-12 23:34:48 +0800

    菜单功能选项

--- 14-golang-IM-system/client.go
+++ 14-golang-IM-system/client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int //当前client的模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 	// 链接server
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
@@ -29,6 +31,47 @@ func NewClient(serverIp string, serverPort int) *Client {
 
 	// 返回对象
 	return client
+}
+
+// 功能项选择
+func (client *Client) menu() bool {
+	var flag int
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scan(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>>请输入合法范围的数字<<<<<<<<<<")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+		}
+		// 根据不同的模式处理不同的业务
+		switch client.flag {
+		case 1:
+			// 公聊模式
+			fmt.Println("公聊模式选择.........")
+			break
+		case 2:
+			// 私聊模式
+			fmt.Println("私聊模式选择.........")
+			break
+		case 3:
+			// 更新用户名
+			fmt.Println("更新用户名选择.........")
+			break
+		}
+	}
 }
 
 var serverIp string
@@ -52,6 +95,7 @@ func main() {
 	fmt.Println(">>>>链接服务器成功。。。")
 
 	// 启动客户端的业务
-	select {}
+	// select {}
+	client.Run()
 
 }
