768e32b37c1c9537088a44ba8c60347f4da7b397
weivang <uquantum@Hotmail.com>
2022-04-12 16:29:41 +0800

    查询在线的用户都有谁

--- 14-golang-IM-system/user.go
+++ 14-golang-IM-system/user.go
@@ -51,9 +51,26 @@ func (this *User) Offline() {
 
 }
 
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg))
+
+}
+
 // 用户处理消息的业务
 func (this *User) DoMessage(msg string) {
-	this.server.BroadCast(this, msg)
+	// 查询指令
+	if msg == "who" {
+		// 查询当前在线用户都有哪些
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.Unlock()
+	} else {
+		this.server.BroadCast(this, msg)
+	}
+
 }
 
 // 监听当前user channel的方法，一旦有消息，就直接发送给对端客户端
