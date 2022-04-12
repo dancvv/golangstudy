9fd9d1b0214fc2e51f9fd5b244eddf23bbb409f2
weivang <uquantum@Hotmail.com>
2022-04-12 21:37:10 +0800

    命令行解析

--- 14-golang-IM-system/client.go
+++ 14-golang-IM-system/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -30,8 +31,20 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+// ./client -ip 127.0.0.1 8888
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器ip地址(默认是127.0.0.1)")
+	flag.IntVar(&serverPort, "port ", 8888, "设置服务器端口(默认是8888)")
+
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	// 命令行解析
+	client := NewClient(serverIp, serverPort)
+	// client := NewClient("127.0.0.1", 8888)
 	if client == nil {
 		fmt.Println(">>>>链接服务器失败。。。。")
 		return
